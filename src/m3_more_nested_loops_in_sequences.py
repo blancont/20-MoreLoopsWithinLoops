70caefe94ff45971b9279c9034add6df0cc75849
blancont <blancont@rose-hulman.edu>
2018-10-17 08:55:40 -0400

    m3

--- src/m3_more_nested_loops_in_sequences.py
+++ src/m3_more_nested_loops_in_sequences.py
@@ -3,8 +3,8 @@ This project demonstrates NESTED LOOPS (i.e., loops within loops)
 in the context of SEQUENCES OF SUB-SEQUENCES.
 
 Authors: David Mutchler, Valerie Galluzzi, Mark Hays, Amanda Stouder,
-         their colleagues and PUT_YOUR_NAME_HERE.
-"""  # TODO: 1. PUT YOUR NAME IN THE ABOVE LINE.
+         their colleagues and Nathaniel Blanco.
+"""  # DONE: 1. PUT YOUR NAME IN THE ABOVE LINE.
 
 
 def main():
@@ -17,7 +17,7 @@ def main():
 def run_test_largest_number():
     """ Tests the    largest_number    function. """
     # ------------------------------------------------------------------
-    # TODO: 2. Implement this TEST function.
+    # DONE: 2. Implement this TEST function.
     #   It TESTS the  largest_number  function defined below.
     #   Include at least ** 1 ** ADDITIONAL test beyond those we wrote.
     # ------------------------------------------------------------------
@@ -44,6 +44,9 @@ def run_test_largest_number():
     print('Expected and actual are:', expected, answer)
 
     # TO DO 2 (continued): Add your ADDITIONAL test(s) here:
+    expect = 0
+    answer = largest_number(([0], [], [-1]))
+    print('Expected and actual are:', expect, answer)
 
 
 def largest_number(seq_seq):
@@ -75,7 +78,14 @@ def largest_number(seq_seq):
     # TODO: 3. Implement and test this function.
     #   Note that you should write its TEST function first (above).
     # ------------------------------------------------------------------
-
+    number = None
+    for k in range(len(seq_seq)):
+        for j in range(len(seq_seq[k])):
+            if number is None:
+                number = seq_seq[k][j]
+            if seq_seq[k][j] > number:
+                number = seq_seq[k][j]
+    return number
 
 def run_test_largest_negative_number():
     """ Tests the    largest_negative_number    function. """
